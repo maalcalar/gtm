5ee2fa0b9ea5d32563da7f40533dabb834b63eaf
Miguel Alcalá <malcala@attachmedia.com>
2020-02-21 12:29:47 -0500

    [INFO]-Avance de clase Project para formar un arreglo de 1 solo trigger y 1 solo tag

--- core/projects/project_classes.js
+++ core/projects/project_classes.js
@@ -1,6 +1,58 @@
+import Trigger from "../triggers/trigger_classes";
+
 export default class Project {
-    constructor(tags = undefined) {
+    constructor(triggers = undefined, tags = undefined) {
+        this._triggers = triggers;
+        this._tags = tags;
+        // this._variables = variables;
+        this._triggersOK = false;
+        this._tagsOK = false;
+        // this._variablesOK = false;
         this._stateOK = false;
+
+        // Revisión y formato de triggers
+        if (this._triggers != undefined) {
+            if (typeof this._triggers === 'object') {
+                if (!this._triggers.length) {
+                    if (this._triggers instanceof Trigger) {
+                        this._triggers = [[this._triggers]];
+
+                        this._triggersOK = true;
+                    } else {
+                        console.log('Warning: Your trigger is not well-formed.');
+                    }
+                } else {
+                    // Revisar arreglo
+                    // Formar un arreglo de triggers
+                }
+            } else {
+                console.log('Warning: Your trigger is not a valid object.');
+            }
+        } else {
+            console.warn('Warning: You did not pass a trigger.');
+        }
+
+        // Revisión y formato de tags
+        if (this._tags != undefined) {
+            if (typeof this._tags === 'object') {
+                if (!this._tags.length) {
+                    if (this._tags instanceof Trigger) {
+                        this._tags = [this._tags];
+
+                        this._tagsOK = true;
+                    } else {
+                        console.log('Warning: Your tag is not well-formed.');
+                    }
+                } else {
+                    // Revisar arreglo
+                    // Formar un arreglo de tags
+                }
+            } else {
+                console.log('Warning: Your tag is not a valid object.');
+            }
+        } else {
+            console.warn('Warning: You did not passed a tag.');
+        }
     }
 
     // Setters
