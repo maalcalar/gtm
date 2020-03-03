d86865208713b7be117bfc72a5cf8e8f0b6697c8
Miguel Alcalá <malcala@attachmedia.com>
2020-03-03 17:19:21 -0500

    [INFO]-Proyecto de prueba

--- app/test_proyecto_01/proyecto_01.project.js
+++ app/test_proyecto_01/proyecto_01.project.js
@@ -1,16 +1,21 @@
+// import 'core-js';
+
 import Project from "../../core/projects/project_classes";
 import trigger from "../Triggers/streamer.trigger";
 import tag from "../Tags/custom_html.tag";
 
 const project = new Project(trigger, tag);
 
-const generador = project.run();
+project.run();
 
-(async function() {
-    for await (let value of generador) {
-        console.log('Iterator value: ', value);
-    }
-})();
+// PRUEBA 2 FUNCIONA BIEN
+// const generador = project.run();
+
+// (async function() {
+//     for await (let value of generador) {
+//         console.log('Iterator value: ', value);
+//     }
+// })();
 
 // PRUEBA 1 FUNCIONA BIEN
 // console.log(generador.next());
